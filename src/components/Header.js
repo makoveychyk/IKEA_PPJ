baa49246d805fda3b6823322c4696da538f59db0
Vlad <makoveychyk@gmail.com>
2019-10-01 13:43:35 +0300

    Add main classes

--- src/components/Header.js
+++ src/components/Header.js
@@ -17,12 +17,13 @@ export default class Header extends Component {
   state = {
     isOpen: false,
     isActive: false,
-    isLogged: false
+    isLogged: false,
+    isNotEmpty: false
   };
 
   render() {
     const { nav, category } = this.props;
-    const { isActive, isLogged, isOpen } = this.state;
+    const { isActive, isLogged, isOpen, isNotEmpty } = this.state;
     const menuClassName = classNames({
       "ikea-menu": true,
       "ikea-menu_open": isOpen === true
@@ -45,12 +46,12 @@ export default class Header extends Component {
     };
     return (
       <>
-        <header className="ikea-header ikea-medium-text ">
+        <header className="ikea-header ikea-medium-text ikea-text-14">
           <div className="ikea-header__inner">
             <div className="container ikea-header__content">
               <nav className="ikea-nav">
                 <button
-                  className="ikea-nav__burger"
+                  className="ikea-icon ikea-icon_lg ikea-nav__burger"
                   onClick={() => this.setState({ isOpen: !isOpen })}
                 >
                   {this.state.isOpen === false ? (
@@ -83,8 +84,7 @@ export default class Header extends Component {
                     </svg>
                   )}
                 </button>
-
-                <a href="#" className="ikea-logo ikea-logo-desk">
+                <a href="/" className="ikea-logo ikea-logo-desk">
                   <svg
                     width="118"
                     height="24"
@@ -155,13 +155,13 @@ export default class Header extends Component {
               </nav>
               <div className="ikea-user-tools">
                 <button
-                  className="ikea-user-tools__btn ikea-user-tools__btn-log"
+                  className="ikea-user-tools__btn ikea-user-tools__btn-log ikea-medium-text ikea-text-14"
                   onClick={() => this.setState({ isLogged: true })}
                 >
                   {isLogged === false ? (
                     "Log In"
                   ) : (
-                    <span className="ikea-user-tools__icon">
+                    <span className="ikea-icon ikea-icon_lg ">
                       <svg
                         width="16"
                         height="21"
@@ -179,14 +179,13 @@ export default class Header extends Component {
                     </span>
                   )}
                 </button>
-                <button className="ikea-user-tools__btn">
+                <button className="ikea-icon ikea-icon_lg ikea-user-tools__btn">
                   <svg
                     width="21"
                     height="21"
                     viewBox="0 0 20 20"
                     fill="#3E444B"
                     xmlns="http://www.w3.org/2000/svg"
-                    className="ikea-user-tools__icon-search"
                   >
                     <path
                       fill-rule="evenodd"
@@ -195,26 +194,30 @@ export default class Header extends Component {
                     />
                   </svg>
                 </button>
-                <button className="ikea-user-tools__btn">
+                <button
+                  className="ikea-icon ikea-icon_xl ikea-user-tools__btn"
+                  onClick={() => this.setState({ isNotEmpty: !isNotEmpty })}
+                >
                   <svg
-                    width="32"
-                    height="32"
-                    viewBox="0 0 34 34"
+                    width="21"
+                    height="21"
+                    viewBox="0 0 21 21"
                     fill="none"
                     xmlns="http://www.w3.org/2000/svg"
                   >
                     <path
                       fill-rule="evenodd"
                       clip-rule="evenodd"
-                      d="M4 24V12H2V10H5C5.55228 10 6 10.4477 6 11V23H18.438L20.438 15H8V13H21.72C22.028 13 22.3188 13.142 22.5083 13.3848C22.6978 13.6276 22.7648 13.9442 22.69 14.243L20.19 24.243C20.0785 24.6879 19.6787 25 19.22 25H5C4.44772 25 4 24.5523 4 24ZM6 31C4.89543 31 4 30.1046 4 29C4 27.8954 4.89543 27 6 27C7.10457 27 8 27.8954 8 29C8 30.1046 7.10457 31 6 31ZM16 29C16 30.1046 16.8954 31 18 31C19.1046 31 20 30.1046 20 29C20 27.8954 19.1046 27 18 27C16.8954 27 16 27.8954 16 29Z"
+                      d="M2 14V2H0V0H3C3.55228 0 4 0.447715 4 1V13H16.438L18.438 5H6V3H19.72C20.028 3.00001 20.3188 3.14196 20.5083 3.38479C20.6978 3.62761 20.7648 3.94422 20.69 4.243L18.19 14.243C18.0785 14.6879 17.6787 15 17.22 15H3C2.44772 15 2 14.5523 2 14ZM4 21C2.89543 21 2 20.1046 2 19C2 17.8954 2.89543 17 4 17C5.10457 17 6 17.8954 6 19C6 20.1046 5.10457 21 4 21ZM14 19C14 20.1046 14.8954 21 16 21C17.1046 21 18 20.1046 18 19C18 17.8954 17.1046 17 16 17C14.8954 17 14 17.8954 14 19Z"
                       fill="#3E444B"
                     />
-                    <rect x="12" width="20" height="20" rx="2" fill="#FFD600" />
-                    <path
-                      d="M19.6738 14H18.2559V7.15039L16.1641 7.86523V6.66406L19.4922 5.43945H19.6738V14ZM28.1992 10.4082C28.1992 11.6348 27.9688 12.5605 27.5078 13.1855C27.0508 13.8066 26.3535 14.1172 25.416 14.1172C24.4941 14.1172 23.7988 13.8145 23.3301 13.209C22.8652 12.5996 22.627 11.6992 22.6152 10.5078V9.03125C22.6152 7.80469 22.8438 6.88477 23.3008 6.27148C23.7617 5.6582 24.4629 5.35156 25.4043 5.35156C26.3379 5.35156 27.0332 5.65039 27.4902 6.24805C27.9512 6.8457 28.1875 7.74023 28.1992 8.93164V10.4082ZM26.7754 8.81445C26.7754 8.01367 26.666 7.42773 26.4473 7.05664C26.2285 6.68164 25.8809 6.49414 25.4043 6.49414C24.9395 6.49414 24.5977 6.67188 24.3789 7.02734C24.1602 7.37891 24.0449 7.92969 24.0332 8.67969V10.6133C24.0332 11.4102 24.1445 12.0039 24.3672 12.3945C24.5898 12.7852 24.9395 12.9805 25.416 12.9805C25.873 12.9805 26.2109 12.8008 26.4297 12.4414C26.6484 12.0781 26.7637 11.5117 26.7754 10.7422V8.81445Z"
-                      fill="white"
-                    />
                   </svg>
+                  <span
+                    className={`ikea-user-tools__basket ikea-main-text ikea-text-12 ${isNotEmpty ===
+                      true && "ikea-user-tools__basket_visible"}`}
+                  >
+                    5
+                  </span>
                 </button>
               </div>
             </div>
@@ -384,6 +387,9 @@ export default class Header extends Component {
               >
                 Sale
               </a>
+              <button className="ikea-btn ikea-btn_sm ikea-btn_blue ikea-main-text ikea-text-14">
+                Click
+              </button>
             </div>
             {/* <div className="ikea-menu__other">
               <div className="ikea-menu__catalog">
